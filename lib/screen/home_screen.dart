140896a49040b49324073b106c951bcae7bf5e19
IbnuTopanAdib <resyaderesya@gmail.com>
2024-09-20 22:39:45 +0700

    add search button

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fruit_shop/const/palette.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('anjing'),
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: 'What do you want',
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(color: Palette.greyColor),
+                    prefixIcon: const Icon(Icons.search),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt))
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
